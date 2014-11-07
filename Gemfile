b1212b6ec5065cffd13b8fc031f217d54a52f95e
Chetan Sarva <chetan@pixelcop.net>
2014-11-07 11:51:06 -0500

    use bixby-bench for allocation stats

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
-source 'http://rubygems.org'
+source 'https://rubygems.org'
 
 gem 'rabl'
 gem 'oj'
 gem 'active_model_serializers'
 gem 'multi_json'
 gem 'ruby-prof'
+gem 'bixby-bench'
--- benchmark.rb
+++ benchmark.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH.unshift(File.expand_path(File.join(File.dirname(__FILE__), 'lib')))
 
-require 'benchmark'
+require 'bixby/bench'
 require 'json_serialization_benchmark'
 
 require 'serializers/event_summary_serializer'
@@ -34,167 +34,129 @@ module SerializationBenchmark
   event_collection = collection_size.times.map { event }
   team_collection  = collection_size.times.map { team }
 
-  Benchmark.benchmark(Benchmark::CAPTION, 40) do |b|
-    sample_size  = 10_000
-    divider_size = 86
+  Bixby::Bench.run(10_000) do |b|
 
-    b.report('RABL Ultra Simple') do
-      sample_size.times do
-        Rabl.render(team, 'teams/item', view_path: rabl_view_path, format: :json)
-      end
+    # ULTRA SIMPLE
+    b.sample('RABL Ultra Simple') do
+      Rabl.render(team, 'teams/item', view_path: rabl_view_path, format: :json)
     end
 
-    b.report('AMS Ultra Simple') do
-      sample_size.times do
-        TeamSerializer.new(team).to_json
-      end
+    b.sample('AMS Ultra Simple') do
+      TeamSerializer.new(team).to_json
     end
 
-    b.report('Presenters Ultra Simple') do
-      sample_size.times do
-        TeamPresenter.new(team).to_json
-      end
+    b.sample('Presenters Ultra Simple') do
+      TeamPresenter.new(team).to_json
     end
 
-    b.report('ApiView Ultra Simple') do
-      sample_size.times do
-        ApiView::Engine.render(team, nil, :format => "json")
-      end
+    b.sample('ApiView Ultra Simple') do
+      ApiView::Engine.render(team, nil, :format => "json")
     end
 
-    puts '-' * divider_size
+
+    # SIMPLE
+    b.divider
 
     b.report('RABL Simple') do
-      sample_size.times do
-        Rabl.render(event, 'events/item', view_path: rabl_view_path, format: :json)
-      end
+      Rabl.render(event, 'events/item', view_path: rabl_view_path, format: :json)
     end
 
     b.report('AMS Simple') do
-      sample_size.times do
-        EventSummarySerializer.new(event).to_json
-      end
+      EventSummarySerializer.new(event).to_json
     end
 
     b.report('Presenters Simple') do
-      sample_size.times do
-        EventSummaryPresenter.new(event).to_json
-      end
+      EventSummaryPresenter.new(event).to_json
     end
 
     b.report('ApiView Simple') do
-      sample_size.times do
-        ApiView::Engine.render(event, nil, :format => "json", :use => EventSummaryApiView)
-      end
+      ApiView::Engine.render(event, nil, :format => "json", :use => EventSummaryApiView)
     end
 
-    puts '-' * divider_size
+
+    # COMPLEX
+    b.divider
 
     b.report('RABL Complex') do
-      sample_size.times do
-        Rabl.render(event, 'basketball/events/show', view_path: rabl_view_path, format: :json)
-      end
+      Rabl.render(event, 'basketball/events/show', view_path: rabl_view_path, format: :json)
     end
 
     b.report('AMS Complex') do
-      sample_size.times do
-        Basketball::EventSerializer.new(event).to_json
-      end
+      Basketball::EventSerializer.new(event).to_json
     end
 
     b.report('Presenters Complex') do
-      sample_size.times do
-        Basketball::EventPresenter.new(event).to_json
-      end
+      Basketball::EventPresenter.new(event).to_json
     end
 
     b.report('ApiView Complex') do
-      sample_size.times do
-        ApiView::Engine.render(event, nil, :format => "json", :use => BasketballEventApiView)
-      end
+      ApiView::Engine.render(event, nil, :format => "json", :use => BasketballEventApiView)
     end
   end
 
-  puts "\n\n"
 
-  Benchmark.benchmark(Benchmark::CAPTION, 40) do |b|
-    sample_size  = 100
-    divider_size = 86
+  # COLLECTION TESTS
+  puts "\n\nCollection tests:\n\n"
 
+  Bixby::Bench.run(100) do |b|
+
+    # ULTRA SIMPLE
     b.report('RABL Ultra Simple: Collection') do
-      sample_size.times do
-        Rabl.render(team_collection, 'teams/index', view_path: rabl_view_path, format: :json)
-      end
+      Rabl.render(team_collection, 'teams/index', view_path: rabl_view_path, format: :json)
     end
 
     b.report('AMS Ultra Simple: Collection') do
-      sample_size.times do
-        ActiveModel::ArraySerializer.new(team_collection, each_serializer: TeamSerializer).to_json
-      end
+      ActiveModel::ArraySerializer.new(team_collection, each_serializer: TeamSerializer).to_json
     end
 
     b.report('Presenters Ultra Simple: Collection') do
-      sample_size.times do
-        team_collection.map { |team| TeamPresenter.new(team).as_json }.to_json
-      end
+      team_collection.map { |team| TeamPresenter.new(team).as_json }.to_json
     end
 
     b.report('ApiView Ultra Simple: Collection') do
-      sample_size.times do
-        ApiView::Engine.render(team_collection, nil, :format => "json")
-      end
+      ApiView::Engine.render(team_collection, nil, :format => "json")
     end
 
-    puts '-' * divider_size
+
+    # SIMPLE
+    b.divider
 
     b.report('RABL Simple: Collection') do
-      sample_size.times do
-        Rabl.render(event_collection, 'events/index', view_path: rabl_view_path, format: :json)
-      end
+      Rabl.render(event_collection, 'events/index', view_path: rabl_view_path, format: :json)
     end
 
     b.report('AMS Simple: Collection') do
-      sample_size.times do
-        ActiveModel::ArraySerializer.new(event_collection, each_serializer: EventSummarySerializer).to_json
-      end
+      ActiveModel::ArraySerializer.new(event_collection, each_serializer: EventSummarySerializer).to_json
     end
 
     b.report('Presenters Simple: Collection') do
-      sample_size.times do
-        event_collection.map { |event| EventSummaryPresenter.new(event).as_json }.to_json
-      end
+      event_collection.map { |event| EventSummaryPresenter.new(event).as_json }.to_json
     end
 
     b.report('ApiView Simple: Collection') do
-      sample_size.times do
-        ApiView::Engine.render(event_collection, nil, :format => "json", :use => EventSummaryApiView)
-      end
+      ApiView::Engine.render(event_collection, nil, :format => "json", :use => EventSummaryApiView)
     end
 
-    puts '-' * divider_size
+
+    # COMPLEX
+    b.divider
 
     b.report('RABL Complex: Collection') do
-      sample_size.times do
-        Rabl.render(event_collection, 'basketball/events/index', view_path: rabl_view_path, format: :json)
-      end
+      Rabl.render(event_collection, 'basketball/events/index', view_path: rabl_view_path, format: :json)
     end
 
     b.report('AMS Complex: Collection') do
-      sample_size.times do
-        ActiveModel::ArraySerializer.new(event_collection, each_serializer: Basketball::EventSerializer).to_json
-      end
+      ActiveModel::ArraySerializer.new(event_collection, each_serializer: Basketball::EventSerializer).to_json
     end
 
     b.report('Presenters Complex: Collection') do
-      sample_size.times do
-        event_collection.map { |event| Basketball::EventPresenter.new(event).as_json }.to_json
-      end
+      event_collection.map { |event| Basketball::EventPresenter.new(event).as_json }.to_json
     end
 
     b.report('ApiView Complex: Collection') do
-      sample_size.times do
-        ApiView::Engine.render(event_collection, nil, :format => "json", :use => BasketballEventApiView)
-      end
+      ApiView::Engine.render(event_collection, nil, :format => "json", :use => BasketballEventApiView)
     end
+
   end
+
 end
